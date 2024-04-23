b5f42d38707b9e34397e39ccd811ed7ca85ee77e
DavidIrankunda <irankundadavid64@gmail.com>
2024-04-23 14:11:43 +0300

    continue building my robofreinds App

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom'; The new way to import createRoot:
+import { createRoot } from "react-dom/client";
 import './index.css';
-import reportWebVitals from './reportWebVitals';
+import App from './containers/App';
 import 'tachyons';
-import App from './App';  
+// import registerServiceWorker from './registerServiceWorker';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
+
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
